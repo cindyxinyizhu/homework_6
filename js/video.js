4a6d8ae0aa305556f9d634ac77bac5e60925a812
cindyxinyizhu <54916958+cindyxinyizhu@users.noreply.github.com>
2020-11-17 16:15:54 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -7,7 +7,6 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
-	console.log(this);
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
@@ -17,46 +16,54 @@ document.querySelector("#pause").addEventListener("click", function() {
 
 document.querySelector("#slower").addEventListener("click", function() {
 	console.log("Slow Down");
-	video.playbackRate = 0.9;
+	video.playbackRate = video.playbackRate*0.9;
+	console.log("New speed is" + video.playbackRate);
 });
 
 
 document.querySelector("#faster").addEventListener("click", function() {
 	console.log("Speed Up");
-	video.playbackRate = 1.1;
+	video.playbackRate = video.playbackRate/0.9;
+	console.log("New speed is" + video.playbackRate);
 });
 
 
 document.querySelector("#skip").addEventListener("click", function() {
 video.currentTime +=5;
 
-if(video.duration <= video.currentTime){
-video.currentTime = 0;
-console.log("Playing from Beginning");}
+	if(video.duration <= video.currentTime){
+		video.currentTime = 0;
+		console.log("Playing from Beginning");}
 
-else{
-	console.log("Skipped to" + video.currentTime);}
-video.play();
+	else{
+		console.log("Skipped to" + video.currentTime);}
 
+		video.play();
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
-if(video.muted == false) {
-	video.muted = true;
-	let mute = document.querySelector("#mute").innerHTML = "Unmute"
-}
+	if(video.muted == false) {
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute"}
 
-else{
-video.muted = false;
-		let mute = document.querySelector("#mute").innerHTML = "mute"
-}
+	else{
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "mute"}
+
 });
 
 document.querySelector("#volumeSlider").addEventListener("change", function() {
+
 	console.log('Before: ' + video.volume);
 	video.volume = this.value / 100;
 	console.log('After: ' + video.volume);
-	let volumeSlider = document.querySelector("#volumeSlider").innerHTML = "volumeSlider"
+
+	let volume = document.querySelector("#volumeSlider").innerHTML = "volume";
+
+    document.getElementById('volumeSlider').value;
+
+
+	console.log("New volume is" + video.volume);
 });
 
 document.querySelector("#old").addEventListener("click", function() {
